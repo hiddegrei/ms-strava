21852cab99fb1b2ef9ccc981d0bc36a41d045536
hiddegrei <grei0005@hz.nl>
2022-12-15 12:44:58 +0100

    activite route

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const cors = require("cors")({ origin: true });
 const app = express();
 app.use(cors);
 const admin = require("firebase-admin");
-
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 admin.initializeApp();
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -14,7 +14,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //exchange auth token for access token
 app.post("/api/users/:userId/token/auth",urlencodedParser, (req, res) => {
-let params = {
+
+    let params = {
   client_id: "74263",
   client_secret: "b388ec2403cbd89e5d10ea582266e30d28361fcb",
   code: req.body.code,
@@ -33,22 +34,149 @@ fetch("https://www.strava.com/oauth/token", {
     //   console.log(json);
 
     if (json.access_token) {
-      res.json({
-        accesToken: json.access_token,
-        expires_at: json.expires_at,
-        refresh_token: json.refresh_token,
-      });
-     admin
-       .firestore()
-       .collection("users")
-       .doc(req.params.userId)
-       .set({ accesToken: json.access_token, expires_at: json.expires_at, refresh_token: json.refresh_token })
-       .catch((err) => console.log(err));
+         admin
+           .firestore()
+           .collection("users")
+           .doc(req.params.userId)
+           .set({ accesToken: json.access_token, expires_at: json.expires_at, refresh_token: json.refresh_token })
+           .catch((err) => console.log(err));
+    //   res.json({
+    //     accesToken: json.access_token,
+    //     expires_at: json.expires_at,
+    //     refresh_token: json.refresh_token,
+    //   });
+    res.json({
+        status:"succes"
+    })
+    
     }
+  }).catch((err)=>{
+    res.json(err)
   });
 });
 
-app.get("/api/users/:userId/token/refresh", (req, res) => {});
+app.get("/api/users/:userId/token/refresh", (req, res) => {
+    let refreshToken="";
+    function resolveAfterEnd() {
+      return new Promise((resolve) => {
+        admin
+          .firestore()
+          .collection("users")
+          .doc(req.params.userId)
+          .get()
+          .then((doc) => {
+           
+            refreshToken=doc.data().refresh_token
+          })
+          .then(() => {
+
+            resolve("succes");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    }
+    async function f1() {
+      const x = await resolveAfterEnd();
+    }
+    async function f2(){
+    let params = {
+      client_id: "74263",
+      client_secret: "b388ec2403cbd89e5d10ea582266e30d28361fcb",
+      code: refreshToken,
+      grant_type: "refresh_token",
+    };
+
+    fetch("https://www.strava.com/oauth/token", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        //   console.log(json);
+
+        if (json.access_token) {
+          admin
+            .firestore()
+            .collection("users")
+            .doc(req.params.userId)
+            .set({ accesToken: json.access_token, expires_at: json.expires_at, refresh_token: json.refresh_token })
+            .catch((err) => console.log(err));
+          //   res.json({
+          //     accesToken: json.access_token,
+          //     expires_at: json.expires_at,
+          //     refresh_token: json.refresh_token,
+          //   });
+          res.json({
+            status: "succes",
+          });
+        }
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+    }
+    f1().then((doc)=>{
+        f2()
+    })
+});
 
 //get activities
-app.get("/api/users/:userId/activities", (req, res) => {});
+app.get("/api/users/:userId/activities", (req, res) => {
+    let accessToken = "";
+    function resolveAfterEnd() {
+      return new Promise((resolve) => {
+        admin
+          .firestore()
+          .collection("users")
+          .doc(req.params.userId)
+          .get()
+          .then((doc) => {
+            accessToken = doc.data().accesToken;
+          })
+          .then(() => {
+            resolve("succes");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    }
+    async function f1() {
+      const x = await resolveAfterEnd();
+    }
+    async function f2() {
+     
+
+      fetch("https://www.strava.com/api/v3/athlete/activities?page=1&per_page=30", {
+        method: "GET", // or 'PUT'
+       headers: {
+        authorization: "Bearer " + accessToken,
+        accept: "application/json",
+      },
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          //   console.log(json);
+
+         
+            res.json({
+              status: "succes",
+              data:json
+            });
+          
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    }
+    f1().then((doc) => {
+      f2();
+    });
+});
+
+exports.app = functions.https.onRequest(app);
